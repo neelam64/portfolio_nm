e2b9b85be054e4be7fa50d33b566ba0110f21de7
Neelam Mangnani <140593536+neelam64@users.noreply.github.com>
2024-06-30 15:09:03 +0000

    g

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Neelam Mangnani",
+  title: "Final Year B.Tech Student",
+  email: "neelammangnani86.com",
+  gitHub: "neelam64",
+  instagram: "neelam_mangnani",
+  linkedIn: "NEELAM MANGNANI",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
