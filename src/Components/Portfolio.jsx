25c50ab10c2df912a71f5535598723f053390306
Neelam Mangnani <140593536+neelam64@users.noreply.github.com>
2024-06-30 17:37:18 +0000

    sa

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,34 +1,8 @@
-/**
- * Portfolio component
- *
- * Highlights some of  your creations. These can be designs, websites,
- * open source contributions, articles you've written and more.
- *
- * This is a great area for you to to continually add to and refine
- * as you continue to learn and create.
- */
-
 import React from "react";
-
-/**
- * Desk image
- *
- * Below is a sample desk image. Feel free to update this to an image of your choice,
- * updating below imageAltText to string that represents what you see in that image.
- *
- * Need an image? Check out https://unsplash.com to download a photo you
- * freely use on your site.
- */
 import image from "../images/design-desk.jpeg";
 
 const imageAltText = "desktop with books and laptop";
 
-/**
- * Project list
- *
- * An array of objects that will be used to display for your project
- * links section. Below is a sample, update to reflect links you'd like to highlight.
- */
 const projectList = [
   {
     title: "10 Things To Know About Azure Static Web Apps 🎉",
@@ -37,29 +11,29 @@ const projectList = [
     url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
   },
   {
-    title: "Web Development for Beginners",
+    title: "LinkedIn Profile",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "My LinkedIn profile where I share my experiences and connect with others.",
+    url: "https://www.linkedin.com/in/neelam-mangnani-51912b22b/",
   },
   {
-    title: "My Resume Site",
+    title: "Python HTTP API Workshop",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "A workshop to help teach how to use Python to create an HTTP API.",
+    url: "https://neelam64.github.io/PythonHTTP_APIWorkshop/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Static Web Apps API Workshop",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "A workshop to help teach how to use Azure Functions to create an API for a Static Web App.",
+    url: "https://neelam64.github.io/mystaticapp/",
   },
 ];
 
 const Portfolio = () => {
   return (
-    <section className="padding" id="portfolio">
-      <h2 style={{ textAlign: "center" }}>Portfolio</h2>
+    <section className="padding portfolio-section" id="portfolio">
+      <h3 style={{ textAlign: "center" }}>Portfolio</h3>
       <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
         <div style={{ maxWidth: "40%", alignSelf: "center" }}>
           <img
